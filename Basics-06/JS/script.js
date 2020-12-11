5a8a91a98f522e3bfdd4b813807fba3915bfd97c
Johns Jijo Sabu <jijojohns12.5@gmail.com>
2020-12-11 11:27:30 +0530

    how to use event listeners

--- Basics-06/JS/script.js
+++ Basics-06/JS/script.js
@@ -6,11 +6,23 @@ const ALERT = document.querySelector("#booking-alert");
 CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
-function reveal (e) {
+function reveal (e, current) {
     // e is event object here and we can hijack the event with preventDefault method.
+
     e.preventDefault();
-    CTA.classList.toggle("hide");
+    current.innerHTML == "Book Now!" ? CTA.innerHTML = "Ooops" : CTA.innerHTML = "Book Now!";
     ALERT.classList.toggle("hide");
 }
 
-CTA.onclick = reveal;
+// CTA.onclick = reveal;
+// CTA.addEventListener("click", reveal, false); // use this when there are no arguments to be passed in reveal function
+
+// use anonymous function while using addEventListener so we can use function inside the anonymous function to add arguments to function
+CTA.addEventListener("click", function (e) {
+    reveal(e, this);
+}, false );
+
+CTA.addEventListener("click", function () {
+    console.log("The button was clicked!");
+}, false);
+
